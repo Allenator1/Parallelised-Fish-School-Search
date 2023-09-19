2e3979c304f8313a003bf43ac36b9ab1cad719c0
Allen <allenantony2001@gmail.com>
2023-09-19 23:08:54 +0800

    seed constant added

--- include/constants.h
+++ include/constants.h
@@ -1,8 +1,9 @@
 #ifndef CONSTANTS_H_
 #define CONSTANTS_H_
 
-#define NUM_ITERATIONS 500
-#define NUM_FISH 10000
+#define SEED 0
+#define NUM_ITERATIONS 5000
+#define NUM_FISH 100000
 #define STEP_IND 0.1
 #define INITIAL_WT 10
 #define LAKE_SIZE 200
