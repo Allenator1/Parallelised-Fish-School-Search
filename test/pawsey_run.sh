b91eedf9ae5ba58ba62a22a0c34b63492f11dba1
Allen <allenantony2001@gmail.com>
2023-09-24 01:46:09 +0800

    sbatch file to run on Setonix

---
+++ test/pawsey_run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --account=courses0101
+#SBATCH --partition=debug
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=00:30:00
+
+cd ../src/
+make all
+cd ../test/
+make driver
+
+srun ./driver -f seq_pawsey.csv -p 1
+srun ./driver -f parallel_pawsey.csv -p 2
+srun ./driver -f parallel_schedules_pawsey.csv -p 2 -s
