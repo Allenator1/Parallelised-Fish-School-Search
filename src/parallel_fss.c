5f498efc1837264f5bb5eccfc7c95e805a446d40
Allen <allenantony2001@gmail.com>
2023-10-24 03:27:34 +0800

    master process will generate and distribute, as well as gather all fish

--- src/parallel_fss.c
+++ src/parallel_fss.c
@@ -38,55 +38,96 @@ int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+    
+    // Create a new MPI datatype for fish 
+    MPI_Datatype mpi_fish_type;
+    MPI_Type_contiguous(5, MPI_FLOAT, &mpi_fish_type);
+    MPI_Type_commit(&mpi_fish_type);
 
+    // Seed the random number generator uniquely for each process and thread
 #pragma omp parallel
 {
     randState = SEED + rank + omp_get_thread_num();
-}
+}   
+    fish *all_fish = NULL;
+    int *fish_counts = NULL;
+    int *fish_offsets = NULL;
 
-    int fish_pproc = args.nfish / num_processes;
+    int nfish_local = args.nfish / num_processes;
     int remainder = args.nfish % num_processes;
     if (rank < remainder) {
-        fish_pproc++;
+        nfish_local++;
     }
-    fish *school = (fish*)malloc(fish_pproc * sizeof(fish));
+    fish *school = (fish*)malloc(nfish_local * sizeof(fish));
     double start_time = omp_get_wtime();
 
+    if (rank == 0) {
+        int curr_offset = 0;
+        all_fish = (fish*)malloc(args.nfish * sizeof(fish));
+        fish_counts = (int*)malloc(num_processes * sizeof(int));
+        fish_offsets = (int*)malloc(num_processes * sizeof(int));
+
+        for (int i = 0; i < num_processes; i++) {
+            fish_offsets[i] = curr_offset;
+            fish_counts[i] = args.nfish / num_processes;
+            if (i < remainder) fish_counts[i]++;
+            curr_offset += fish_counts[i];
+        }
+
 #pragma omp parallel for schedule(runtime)
-    for (int i = 0; i < fish_pproc; i++) {
+        for (int i = 0; i < args.nfish; i++) {
+            fish f;
+            init_fish(&f, &randState);
+            all_fish[i] = f;
+        } 
+    }
+
+    MPI_Scatterv(all_fish, fish_counts, fish_offsets, mpi_fish_type, 
+                 school, nfish_local, mpi_fish_type, 0, MPI_COMM_WORLD);    
+
+
+#pragma omp parallel for schedule(runtime)
+    for (int i = 0; i < nfish_local; i++) {
         fish f;
         init_fish(&f, &randState);
         school[i] = f;
     }
 
-#pragma omp parallel
-{
+    float max_df; 
+    float sum_wt; 
+    float sum_xwt; 
+    float sum_ywt;
+
+#pragma omp parallel shared(max_df, sum_wt, sum_xwt, sum_ywt)
+{   
     for (int i = 0; i < args.nrounds; i++) {
-        float max_df = 0;
-        float sum_wt = 0;
-        float sum_xwt = 0;      // sum of x * wt
-        float sum_ywt = 0;      // sum of y * wt
 
 #pragma omp for schedule(runtime) reduction(max:max_df)
         // Random swimming by fish
-        for (int j = 0; j < fish_pproc; j++) {
+        for (int j = 0; j < nfish_local; j++) {
             swimfish(&school[j], &randState, STEP_IND);
-            max_df = (max_df, school[j].df);
+            max_df = fmax(max_df, school[j].df);
         }
 
 #pragma omp master
         MPI_Allreduce(MPI_IN_PLACE, &max_df, 1, MPI_FLOAT, MPI_MAX, MPI_COMM_WORLD);
-#pragma omp barrier
+
+#pragma omp single
+{
+        sum_wt = 0;
+        sum_xwt = 0;      // sum of x * wt
+        sum_ywt = 0;      // sum of y * wt
+}
 
 #pragma omp for schedule(runtime) nowait
         // Feeding 
-        for (int j = 0; j < fish_pproc; j++) {
+        for (int j = 0; j < nfish_local; j++) {
             feedfish(&school[j], max_df);
         }
 
 #pragma omp for schedule(runtime) reduction(+: sum_wt, sum_xwt, sum_ywt)
         // Calculate the barycenter as the weighed average of fish positions
-        for (int j = 0; j < fish_pproc; j++) {
+        for (int j = 0; j < nfish_local; j++) {
             sum_wt += school[j].wt;
             sum_xwt += school[j].x * school[j].wt;
             sum_ywt += school[j].y * school[j].wt;
@@ -95,32 +136,26 @@ int main(int argc, char *argv[]) {
 
 #pragma omp master
         MPI_Allreduce(MPI_IN_PLACE, &bary_sums, 3, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
-#pragma omp barrier
+
+#pragma omp single
+        max_df = 0;
 
         float bari_x = bary_sums[0] / bary_sums[2];
         float bari_y = bary_sums[1] / bary_sums[2];
         float bari = sqrt(bari_x * bari_x + bari_y * bari_y);
     }
 }
-    // Gather all fish to rank 0 and print the lake if verbose option is set
-    if (args.verbose) {
-        fish *mega_school = NULL;
-        int nfloat_pproc = fish_pproc * sizeof(fish) / sizeof(float);
-        int nfloat = args.nfish * sizeof(fish) / sizeof(float);
-
-        if (rank == 0) {
-            fish *mega_school = (fish*)malloc(args.nfish * sizeof(fish));
-        }
-
-        MPI_Gather(school, nfloat_pproc, MPI_FLOAT, 
-                    mega_school, nfloat, MPI_FLOAT, 0, 
-                    MPI_COMM_WORLD);
+    // Gather all fish to rank 0
+    MPI_Gatherv(school, nfish_local, mpi_fish_type, 
+                all_fish, fish_counts, fish_offsets,
+                mpi_fish_type, 0, MPI_COMM_WORLD);
 
         if (rank == 0) { 
-            print_lake(mega_school, args.gui_grid_size);
-            free(mega_school);
+            if (args.verbose) print_lake(all_fish, args.gui_grid_size, args.nfish);
+            free(all_fish);
+            free(fish_counts);
+            free(fish_offsets);
         }
-    }
     free(school);
 
     // Print the time taken to run the program
